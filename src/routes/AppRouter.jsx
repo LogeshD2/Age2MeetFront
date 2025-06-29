553460444c6156deffe4508901e2e25d23a06dd1
Fasil Mougamadou <mougamadou.fasil@gmail.com>
2025-06-29 11:27:57 +0200

    Ajout page messagerie

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,6 +5,7 @@ import MainLayout from '../layout/MainLayout';
 import Home from '../pages/Home';
 import Inscription from '../pages/Inscription';
 import Profil from '../pages/Profil';
+import Messagerie from '../pages/Messagerie';
 
 const AppRouter = () => {
   return (
@@ -13,7 +14,7 @@ const AppRouter = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="inscription" element={<Inscription />} />
-          <Route path="messagerie" element={<div>Page Messagerie (à développer)</div>} />
+          <Route path="messagerie" element={<Messagerie />} />
           <Route path="agenda" element={<div>Page Mon agenda (à développer)</div>} />
           <Route path="profil" element={<Profil />} />
           <Route path="contact" element={<div>Page Contact (à développer)</div>} />
