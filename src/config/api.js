034e9e5cef209d35a32a9fe93a4e089767581488
LogeshD2 <logesh.djearam02@gmail.com>
2025-06-30 23:02:27 +0200

    SERVEUR

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://age2meet.onrender.com/api';
+const API_BASE_URL = 'https://age2meet.onrender.com/api/docs';
 
 export const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem('authToken');
